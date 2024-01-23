01008febbabbba4f3a591c3d014c213ab3fe32b4
Rémy Tuyéras <36420536+remytuyeras@users.noreply.github.com>
2024-01-23 16:45:05 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,78 @@
+<p align="center">
+  <!-- <img width="300px" src="img/logo.png" /> -->
+  <h1 align="center">ACES</a></h1>
+  <p align="center">A python library for the fully homomorphic encryption scheme ACES</p>
+</p>
+
+# Presentation
+
+This package proposes an implementation for the ACES cryptosystem where we take the paramter $\omega = 1$, $N=1$ and $\mathsf{deg}(u) = n$. Details are available in the preprint <em>"Constructing a Fully Homomorphic Encryption Scheme with the Yoneda Lemma"</em>.
+
+# Installation
+
+At this time, the only way to install the package is to download its code and use it in your own code as follows.
+
+```python
+from aces import *
+```
+
+# Quickstart
+
+To create an arithmetic channel $\mathsf{C} = (q,\omega,u)$, use the class ```ArithChannel```. The following snippet shows how to generate an arithmetic channel with $q=27$ and $\mathsf{deg}(u) = 10 = n$.
+```python
+>>> from aces import *
+>>> ac = ArithChannel(27,10)
+```
+To generate the public key of a scheme with fully homomorphic properties, use the following instance:
+```python
+>>> (f0,f1,intmod,dim,u,tensor) = ac.publish(fhe = True)
+```
+To encrypt messages $m \in \mathbb{Z}_q$, use the class ```ACES``` as shown below.
+```python
+>>> bob = ACES(f0,f1,intmod,dim,u)
+```
+The encryption of the messages $3$ and $5$ is done as follows:
+```python
+>>> c_a = bob.encrypt(3)
+>>> print(c_a[1])
+[15]^9+[8]^8+[6]^7+[16]^6+[11]^5+[22]^4+[14]^3+[14]^2+[14]^1+[8]^0 (27)
+>>> c_b = bob.encrypt(5)
+>>> print(c_b[1])
+[11]^9+[9]^8+[2]^7+[3]^6+[6]^5+[22]^4+[16]^3+[19]^2+[13]^1+[20]^0 (27)
+```
+To do arithmetic on encrypted information, use the class ```ACESAlgebra```. For example, we have:
+```
+>>> alg = ACESAlgebra(intmod,dim,u,tensor)
+>>> alice = ACESReader(ac.x,intmod,dim,u)
+>>> alice.decrypt(alg.add(c_a,c_b))
+8
+>>> alice.decrypt(alg.mult(c_a,c_b))
+15
+```
+A canonical attack for the ACES cryptosystem is the evaluation of the encryption polynomial at $\omega=1$. For most parameterizations, we can verify that this attack collects what empirically appears to be a random distribution.
+
+```python
+# Setup the cryptosystem parameters
+ac = ArithChannel(27,10)
+(f0,f1,intmod,dim,u,tensor) = ac.publish(fhe = True)
+# Simulate two communicating parties
+bob = ACES(f0,f1,intmod,dim,u)
+alice = ACESReader(ac.x,intmod,dim,u)
+
+# Compute the polynomial evaluations Enc(m)(omega) where omega=1
+dist = []
+m = 3
+for i in range(2000):
+  c = bob.encrypt(m)
+  attack = sum(c[1].coefs) % intmod
+  d = alice.decrypt(c)
+  print(f"Enc({m})(omega) =",c[1],"attack:",attack,"[Sanity check] decrypted as",d)
+  dist.append(attack)
+
+plt.hist(dist, density=False, bins=range(intmod+1))
+plt.ylabel('Probability')
+plt.xlabel('Value obtained from simple attack')
+plt.show()
+
+```
+![](img/distribution.png) 
---
+++ aces.py
@@ -0,0 +1,313 @@
+def degree(p):
+  for i,c in enumerate(p.coefs[::-1]):
+    if c != 0:
+      return len(p.coefs)-i-1
+  return 0
+
+class Polynomial(object):
+  
+  def __init__(self,coefs,intmod=None):
+    self.coefs = coefs
+    self.intmod = intmod
+
+  def __repr__(self):
+    return "+".join([f"[{c}]^{k}" for k,c in enumerate(self.coefs) if k <= degree(self) and c != 0][::-1])+f" ({self.intmod})"
+
+  def mod(self,intmod=None):
+    if intmod == None:
+      return Polynomial(self.coefs,None)
+    else:
+      return Polynomial([(x % intmod) for x in self.coefs],intmod)
+
+  def __mul__(self,other):
+    mod = None if self.intmod != other.intmod else self.intmod
+    mod_coef = lambda x,y: (x * y) % mod if mod != None else x*y
+    d_self = degree(self)
+    d_other = degree(other)
+    coefs = [sum([(mod_coef(c,other.coefs[n-k]) if 0 <= n-k < len(other.coefs) else 0) for k,c in enumerate(self.coefs)]) for n in range(d_self+d_other+1)]
+    return Polynomial(coefs,mod)
+
+  def __add__(self,other):
+    mod = None if self.intmod != other.intmod else self.intmod
+    mod_coef = lambda x,y: (x + y) % mod if mod != None else x + y
+    d_self = degree(self)
+    d_other = degree(other)
+    coefs = [ mod_coef(self.coefs[k] if 0<=k<len(self.coefs) else 0, other.coefs[k] if 0 <=k<len(other.coefs) else 0) for k in range(max(d_self,d_other)+1)]
+    return Polynomial(coefs,mod)
+  
+  def __lshift__(self,other):
+    d_other = degree(other)
+    if other.coefs[d_other] != 1:
+      print("Warning for Polynomial.__lshift__: polynomial modulus is not monic (no action taken)")
+      return self, False
+
+    d_self = degree(self)
+    if d_self < d_other:
+      return self, False
+
+    deg_diff = d_self-d_other
+    a_d = self.coefs[d_self]
+    mod = None if self.intmod != other.intmod else self.intmod
+    mod_coef = lambda x,y: (x - a_d * y) % mod if mod != None else x - a_d * y
+    coefs = [(mod_coef(c,other.coefs[k-deg_diff]) if  0 <= k-deg_diff < len(other.coefs) else c) for k,c in enumerate(self.coefs)]
+    return Polynomial(coefs,mod), True
+
+  def __mod__(self,other):
+    p, t = self << other
+    while t:
+      p, t = p << other
+    return p
+
+
+import math
+import random
+
+def extended_gcd(a, b):
+    r = [a,b]
+    s = [1,0]
+    t = [0,1]
+    while not(0 in r):
+      r1 = r[-1]
+      r0 = r[-2]
+      q, r2 = divmod(r0,r1)
+      r.append(r2)
+      s.append(s[-2]-q*s[-1])
+      t.append(t[-2]-q*t[-1]) 
+    return (r[-2],s[-2],t[-2])
+
+def randinverse(intmod):
+  a = random.randrange(1,intmod)
+  while math.gcd(a,intmod) != 1:
+    a = random.randrange(1,intmod)
+  _, inva, k = extended_gcd(a,intmod)
+  return (a,inva % intmod)
+
+
+import numpy as np
+
+class RandIso(object):
+
+  def __init__(self,intmod,dim):
+    self.intmod = intmod
+    self.dim = dim
+
+  def generate_pair(self):
+    i = random.randrange(self.dim)
+    j = random.choice([k for k in range(self.dim) if k!=i])
+    return i,j
+
+  def generate_swap(self):
+    i,j = self.generate_pair()
+    m = []
+    for r in range(self.dim):
+      if r == i:
+        m.append([(1 if s==j else 0) for s in range(self.dim)])
+      elif r == j:
+        m.append([(1 if s==i else 0) for s in range(self.dim)])
+      else:
+        m.append([(1 if s==r else 0) for s in range(self.dim)])
+    return np.array(m)
+        
+  def generate_mult(self):
+    m = []
+    invm = []
+    for r in range(self.dim):
+      a,inva = randinverse(self.intmod)
+      m.append([(a if s==r else 0) for s in range(self.dim)])
+      invm.append([(inva if s==r else 0) for s in range(self.dim)])
+    return np.array(m), np.array(invm)
+
+  def generate_line(self):
+    i,j = self.generate_pair()
+    m = []
+    invm = []
+    a = random.randrange(1,self.intmod)
+    for r in range(self.dim):
+      m.append([(1 if s==r else ( a if r==i and s==j else 0)) for s in range(self.dim)])
+      invm.append([(1 if s==r else ( self.intmod-a if r==i and s==j else 0)) for s in range(self.dim)])
+    return np.array(m), np.array(invm)
+
+  def generate(self,length,pswap=1,pmult=2,pline=3):
+    u = np.eye(self.dim, dtype=int)
+    invu = np.eye(self.dim, dtype=int)
+    choices = ["swap"] * pswap + ["mult"] * pmult + ["line"] * pline
+    for i in range(length):
+      x = random.choice(choices)
+      if x == "swap":
+        m = invm = self.generate_swap()
+      if x == "mult":
+        m , invm = self.generate_mult()
+      if x == "line":
+        m , invm = self.generate_line()
+      u = (m @ u) % self.intmod
+      invu = (invu @ invm) % self.intmod
+    return (u % self.intmod), (invu % self.intmod)
+
+
+
+class ArithChannel(object):
+
+  #dim is both n = dim(x) and degre(u)
+  def __init__(self,intmod,dim):
+    # Implicit values for omega and N:
+    self.omega = 1
+    self.N = 1
+    # Generated values for q, n, u, x, f0
+    self.intmod = intmod
+    self.dim = dim
+    self.u = self.generate_u()
+    self.x, self.tensor = self.generate_secret(self.u)
+    self.f0 = self.generate_initializer()
+
+  def publish(self,fhe = False):
+    f1_pre = Polynomial([0],self.intmod)
+    for i in range(self.dim):
+      f1_pre = f1_pre + self.f0[i] * self.x[i]
+    f1 = f1_pre % self.u
+    if fhe:
+      return (self.f0,f1,self.intmod,self.dim,self.u,self.tensor)
+    else:
+      return (self.f0,f1,self.intmod,self.dim,self.u)
+
+  def generate_u(self):
+    #number of non zero coefficients for u
+    nonzeros = max(2,min(self.dim-1,int(random.gauss(self.intmod/2,self.intmod/2))))
+    
+    #The dominant coefficient for u is equal to 1
+    u_coefs = [1]
+
+    while nonzeros-len(u_coefs) > 1:
+      a, _ = randinverse(self.intmod)
+      u_coefs.append(a)
+
+    u_coefs.append(self.intmod - sum(u_coefs))
+    #number of zero coefficients for u
+    zeros = self.dim - len(u_coefs)
+
+    decomp = []
+    remaining = zeros-sum(decomp)
+    while remaining > 0:
+      samp = max(0,min(remaining,int(random.gauss(remaining/2,remaining/2))))
+      decomp.append(random.randint(0,samp))
+      remaining = zeros-sum(decomp)
+    
+    u = []
+    for i in range(len(u_coefs)):
+      u.append(u_coefs[i])
+      if i < len(decomp):
+        u.extend([0] * decomp[i])
+    u.extend([0] * (self.dim - len(u)+1))
+    
+    return Polynomial(u[::-1],self.intmod)
+
+
+  def generate_secret(self,poly_u):
+    ri = RandIso(self.intmod,self.dim)
+    m, invm = ri.generate(60)
+
+    x = []
+    m_t = np.transpose(m)
+    for k in range(len(m)):
+      x.append(Polynomial(list(m_t[k]),self.intmod))
+
+    tensor = []
+    # we will have a list/array: tensor[i][j][k]
+    for i in range(len(x)):
+      row = []
+      for j in range(len(x)):
+        xi_xj_mod_u = (x[i] * x[j]) % poly_u
+        a_ij_poly = xi_xj_mod_u.mod(self.intmod)
+
+        if sum(a_ij_poly.coefs[self.dim:]) != 0:
+          print("Error in ArithChannel.generate_secret: tensor cannot be computed due to dimension discrepancies")
+          print(a_ij_poly)
+          exit()
+
+        a_ij = np.array(a_ij_poly.coefs[:self.dim] + [0] * (self.dim - len(a_ij_poly.coefs)) )
+        row.append((invm @ a_ij) % self.intmod)
+      tensor.append(np.array(row))
+    
+    return x, np.array(tensor)
+
+  def generate_initializer(self):
+    f0 = []
+    for i in range(self.dim):
+      f0.append(Polynomial([random.randrange(self.intmod) for _ in range(self.dim)],self.intmod))
+    return f0
+
+# Arithmetic Channel Ecnryption Scheme 
+class ACES(object):
+
+  def __init__(self,f0,f1,intmod,dim,u):
+    self.f0 = f0
+    self.f1 = f1
+    self.intmod = intmod
+    self.dim = dim
+    self.u = u
+
+  def encrypt(self,m):
+    if m >= self.intmod:
+      print(f"Warning in ACES.encrypt: the input is equivalent to {m % self.intmod}")
+    r_m = self.generate_error(m)
+    e = self.generate_vanisher()
+    b = Polynomial([random.randrange(self.intmod) for _ in range(self.dim)],self.intmod)
+    c = []
+    for i in range(self.dim):
+      c.append( (b * self.f0[i] ) % self.u )
+    return (c , (r_m + b * self.f1 + e) % self.u)
+
+  def generate_vanisher(self):
+    e_res = Polynomial([random.randrange(self.intmod) for _ in range(self.dim)],self.intmod)
+    shift = Polynomial([self.intmod - sum(e_res.coefs)],self.intmod)
+    return shift + e_res
+
+  def generate_error(self,m):
+    r_m_res = Polynomial([random.randrange(self.intmod) for _ in range(self.dim)],self.intmod)
+    shift = Polynomial([m - sum(r_m_res.coefs)],self.intmod)
+    return shift + r_m_res
+
+
+class ACESReader(object):
+
+  def __init__(self,x,intmod,dim,u):
+    self.x = x
+    self.intmod = intmod
+    self.dim = dim
+    self.u = u
+
+  def decrypt(self,c):
+    c0Tx = Polynomial([0],self.intmod)
+    for i in range(self.dim):
+      c0Tx = c0Tx + c[0][i] * self.x[i]
+    
+    m_pre = c[1] + Polynomial([-1],self.intmod) * c0Tx
+    return sum(m_pre.coefs) % self.intmod
+
+
+class ACESAlgebra(object):
+
+  def __init__(self,intmod,dim,u,tensor):
+    self.intmod = intmod
+    self.dim = dim
+    self.u = u
+    self.tensor = tensor
+
+  def add(self,a,b):
+    c0 = [ (a[0][k]+b[0][k]) % self.u for k in range(self.dim) ]
+    c1 = (a[1]+b[1]) % self.u
+    return (c0, c1)
+  
+  def mult(self,a,b):
+    t = []
+    for k in range(self.dim):
+      tmp = Polynomial([0],self.intmod)
+      for i in range(self.dim):
+        for j in range(self.dim):
+          tmp = tmp + Polynomial([self.tensor[i][j][k]],self.intmod) * a[0][i] * b[0][j]
+      t.append(tmp)
+
+    c0 = [ ( b[1] * a[0][k] +a[1] * b[0][k] + Polynomial([-1],self.intmod) * t[k]) % self.u for k in range(self.dim) ]
+    c1 = ( a[1] * b[1] ) % self.u
+    return (c0, c1)
+
+
